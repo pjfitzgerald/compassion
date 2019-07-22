d7c81d9f6c03d3aaa7e793787e7e2c8c43c6aa3b
Patrick Fitzgerald <patrick.fitzgerald29@gmail.com>
2019-07-22 18:09:59 +0200

    detailed seeds in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,32 @@ users = [
   {
     email: "spiderman@spiderman.com",
     password: "spiderman"
+  },
+  {
+    email: "ironman@ironman.com",
+    password: "ironman"
+  },
+  {
+    email: "hulk@hulk.com",
+    password: "hulk"
+  },
+  {
+    email: "robin@robin.com",
+    password: "robin"
   }]
 
-  users.each do |user|
-    user = User.create!(email: user[:email], password: user[:password])
-  end
+test_messages = ["testing 1234", "nice to meet you", "what was the best thing that happened today for you?" "hello", "how are you feeling?", "thanks for your help"]
 
-  puts "Created #{User.count} users."
+users.each do |user|
+  user = User.create!(email: user[:email], password: user[:password])
+end
+
+test_messages.each do |message|
+  message = Message.create!(content: test_messages.sample)
+end
+
+
+puts "Created #{User.count} users."
+puts "Created #{Match.count} matches."
+puts "Created #{Chatroom.count} chatrooms."
+puts "Created #{Message.count} messages."
