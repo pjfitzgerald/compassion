d37bf426619e347bbd0c7adf5e5dc89bc07fb15b
Nathan Kim <nakim994@gmail.com>
2019-07-22 16:40:48 +0200

    homepage styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @question = ["Do you like sports?", "Do you enjoy cooking?", "Do you sing in the shower?"].sample
   end
 end
