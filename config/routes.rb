8e133b906174a953b8c4a0a13f7188f6a0da3526
Nathan Kim <nakim994@gmail.com>
2019-07-23 17:02:49 +0200

    added signup inputs and linked homepage buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   resources :users do
     resources :chatrooms, only: [:show]
+    get "survey", to: "users#survey", as: :survey
   end
   # add journal here?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_122457) do
+ActiveRecord::Schema.define(version: 2019_07_23_145239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,10 @@ ActiveRecord::Schema.define(version: 2019_07_22_122457) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "answered"
+    t.string "age_range"
+    t.string "gender"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
