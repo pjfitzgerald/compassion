d3bbca06aff1a043cb825059914a769cfefe950e
Nathan Kim <nakim994@gmail.com>
2019-07-23 17:31:56 +0200

    redirected after signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,19 @@
 class UsersController < ApplicationController
-
   before_action :set_user, only: [:show, :new, :update]
   before_action :authenticate_user!, only: [:update]
 
   def show
   end
 
-  def update
-    @user.update(age_range: params[:user][:age_range], gender: params[:user][:gender])
-    if @user.save!
-      @user.update!(answered: true)
-      redirect_to user_path(@user)
-    else
-      render :survey
-    end
-  end
+  # def update
+  #   @user.update(age_range: params[:user][:age_range], gender: params[:user][:gender])
+  #   if @user.save!
+  #     @user.update!(answered: true)
+  #     redirect_to user_path(@user)
+  #   else
+  #     render :survey
+  #   end
+  # end
 
   def survey
     @user = User.find(params[:user_id])
@@ -25,6 +24,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:age_range, :user, :gender)
