210c29d38751f2d9f8abf359d620f43d670cca36
Patrick Fitzgerald <patrick.fitzgerald29@gmail.com>
2019-07-23 11:31:37 +0200

    some seed updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require 'open-uri'
+require 'nokogiri' ingredient = 'chocolate'
+url = "http://www.letscookfrench.com/recipes/find-recipe.aspx"
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+html_doc.search('.m_titre_resultat a').each do |element| puts element.text.strip
+puts element.attribute('href').value
+end
+
+
+
 puts "Cleaning up database"
 User.destroy_all
 
