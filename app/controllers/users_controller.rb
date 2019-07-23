f751db806bc0b527a9ef220aac08872a60618833
William Wik <william.wik@isobar.com>
2019-07-23 15:10:06 +0200

    Created sidebar component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user
-  
   def show
-  end
 
-  def new
-    @user = User.new
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
   end
 end
