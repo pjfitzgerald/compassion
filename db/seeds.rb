777185a0ae36e0acdcee0121b7cafda582fcb7f9
Patrick Fitzgerald <patrick.fitzgerald29@gmail.com>
2019-07-23 15:04:28 +0200

    seeds complete except for messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,20 @@
-require 'open-uri'
-require 'nokogiri' ingredient = 'chocolate'
-url = "http://www.letscookfrench.com/recipes/find-recipe.aspx"
-html_file = open(url).read
-html_doc = Nokogiri::HTML(html_file)
-html_doc.search('.m_titre_resultat a').each do |element| puts element.text.strip
-puts element.attribute('href').value
-end
-
+# require 'open-uri'
+# require 'nokogiri' ingredient = 'chocolate'
+# url = "http://www.letscookfrench.com/recipes/find-recipe.aspx"
+# html_file = open(url).read
+# html_doc = Nokogiri::HTML(html_file)
+# html_doc.search('.m_titre_resultat a').each do |element| puts element.text.strip
+# puts element.attribute('href').value
+# end
 
 
 puts "Cleaning up database"
 User.destroy_all
-
+Match.destroy_all
+Chatroom.destroy_all
+Message.destroy_all
 puts "Creating new database"
+
 users = [
   {
     email: "batman@batman.com",
@@ -29,14 +31,6 @@ users = [
   {
     email: "ironman@ironman.com",
     password: "ironman"
-  },
-  {
-    email: "hulk@hulk.com",
-    password: "hulk"
-  },
-  {
-    email: "robin@robin.com",
-    password: "robin"
   }]
 
 test_messages = ["testing 1234", "nice to meet you", "what was the best thing that happened today for you?" "hello", "how are you feeling?", "thanks for your help"]
@@ -45,9 +39,17 @@ users.each do |user|
   user = User.create!(email: user[:email], password: user[:password])
 end
 
-test_messages.each do |message|
-  message = Message.create!(content: test_messages.sample)
-end
+matched_users = User.limit(2)
+Match.create!(user: matched_users[0], partner: matched_users[1])
+Chatroom.create!(match: Match.first)
+
+# matches = Match.all
+# matches.each do |match|
+#   test_messages.each do |message|
+#     Message.create!(user: match.user, chatroom: Chatroom.find_by(match_id: match.id), content: message)
+#   end
+# end
+
 
 
 puts "Created #{User.count} users."
