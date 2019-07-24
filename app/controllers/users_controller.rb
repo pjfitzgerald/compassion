a0a761035c45f6724de4fec64b6888c7171d390f
Nathan Kim <nakim994@gmail.com>
2019-07-24 10:16:14 +0200

    sampled questions into questionbox

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
-require 'open-uri'
-require 'nokogiri'
+require 'csv'
 
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :new, :update]
   before_action :authenticate_user!, only: [:update]
 
   def show
-
+    filepath = File.join(Rails.root, 'config', 'questions.csv')
+    @questions = []
+    CSV.foreach(filepath) do |row|
+      unless row.empty?
+        unless row[1].nil?
+        @questions << row[1] unless row[1].empty?
+      end
+      end
+    end
   end
 
   # def update
