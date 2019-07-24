567a7b00f8b87e43c7a2451907fa6604c7196b47
Benjamin Baranger <baranger.benjamin@gmail.com>
2019-07-24 15:46:01 +0100

    fix action cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,12 +5,13 @@ class Message < ApplicationRecord
   after_create :broadcast_message
 
   def broadcast_message
-    ActionCable.server.broadcast("chatroom_#{chatroom.id}", {
-      message_partial: ApplicationController.renderer.render(
-        partial: "messages/message",
-        locals: { message: self, user_is_messages_author: false }
-      ),
-      current_user_id: user.id
-    })
+    ActionCable.server.broadcast("chatroom_#{chatroom.id}",
+      {
+        message_partial: ApplicationController.renderer.render(
+          partial: "messages/message",
+          locals: { message: self, user_is_messages_author: false }
+        ),
+        current_user_id: user.id
+      })
   end
 end
