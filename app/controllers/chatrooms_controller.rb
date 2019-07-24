1b5c4763a36cffaddfc78621e5d9d041ca47dd6f
Patrick Fitzgerald <patrick.fitzgerald29@gmail.com>
2019-07-24 12:07:28 +0200

    chat working without auto refresh

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,10 @@
 class ChatroomsController < ApplicationController
 
   def show
-    @chatroom = Chatroom.find(params[:id])
+    @chatroom = Chatroom.includes(messages: :user).find(params[:id])
+    @user = current_user
+    @messages = @chatroom.messages.order(created_at: :asc).last(20)
+    @message = Message.new
     # raise
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,33 @@
 class MessagesController < ApplicationController
-  def create
+  # before_action :set_message, only: [:create]
 
+  def create
+    @message = Message.new(message_params)
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message.user = current_user
+    @message.chatroom = @chatroom
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to chat_room_path(@chat_room) }
+        format.js
+      end
+    else
+      redirect_to chatroom_path(params[:id])
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+    authenticate @message
+  end
+
+  def set_chatroom
+    @chatroom = Chatroom.includes(messages: :user).find(params[:id])
   end
 end
