6da0bd14451e2b2f1f5e3b3b46565564d4534803
Benjamin Baranger <baranger.benjamin@gmail.com>
2019-07-24 15:46:01 +0100

    fix action cable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,20 +1,12 @@
 import "bootstrap";
 import { expandOrMinify } from "../components/sidebar";
-import { scrollLastMessageIntoView } from "../components/scroll";
-
-
-
-
-
-
-scrollLastMessageIntoView();
 
 
 let button = document.querySelector(".button-expand-large");
 if (button) {
   button.addEventListener("click", () => {
     expandOrMinify();
-  })  
+  })
 }
 
 
