5329cb6729bb767eccdf1eb90ec549cd1f9ad3c6
Benjamin Baranger <baranger.benjamin@gmail.com>
2019-07-25 15:09:42 +0100

    add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  require "sidekiq/web"
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
   devise_for :users, controllers: { registrations: "registrations", sessions: "sessions" }
   root to: 'pages#home'
   resources :users do
