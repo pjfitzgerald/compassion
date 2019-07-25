d7e4e5b1baadc8452c1255fd580a017d96a2156e
Nathan Kim <nakim994@gmail.com>
2019-07-25 10:42:42 +0200

    completed form with minimal frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations", sessions: "sessions" }
   root to: 'pages#home'
-  resources :users do
-    get "survey", to: "users#survey", as: :survey
-  end
+  resources :users
+  get "survey", to: "users#survey", as: :survey
   resources :chatrooms, only: [:show] do
     resources :messages, only: [:create]
   end
