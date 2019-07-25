afc7c0cd6ada96470bd6d1a3e00454e0c2ec2aa1
Patrick Fitzgerald <patrick.fitzgerald29@gmail.com>
2019-07-25 15:46:58 +0200

    removed login/signup buttons for logged in users

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,23 @@
 class MatchesController < ApplicationController
   def new
     @match = Match.new
+    @other_user = User.find_by(category: current_user.category, searching: true)
+    if current_user.id > @other_user.id
+      @match.user_id = current_user.id
+      @match.partner_id = @other_user.id
+    else
+      @match.user_id = @other_user.id
+      @match.partner_id = current_user.id
+    end
   end
 
   def create
     @match = Match.new
     User.all.each
   end
+
+  def categorise
+    @category
+  end
+
 end
