7c49d5590dbaeb7590ab6218100c6c3263ebd48d
William Wik <william.wik@isobar.com>
2019-07-25 19:10:45 +0200

    Added A LOT of stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :new]
+  before_action :set_user, only: [:show, :new, :username_pick]
   before_action :authenticate_user!, only: [:update]
 
   def show
@@ -19,22 +19,80 @@ class UsersController < ApplicationController
   def update
     @collection = ["n/a", "1", "2", "3", "4", "5"]
     @user = current_user
-    @user.update(answer_one: params[:user][:answer_one], answer_two: params[:user][:answer_two],
-                 answer_three: params[:user][:answer_three], answer_four: params[:user][:answer_four],
-                 answer_five: params[:user][:answer_five])
-    if @user.answer_one.zero? || @user.answer_two.zero? ||
-       @user.answer_three.zero? || @user.answer_four.zero? ||
-       @user.answer_five.zero?
-      @user.update(answered: false)
-    else
-      @user.update(answered: true)
-    end
-    if @user.save
+    # if previous page was survey
+    #   save answer params to current_user
+    # => and redirect to customize_user_path
+    # elsif previous page was profile input page
+    # => create a new profile and set current user as profile.user_id
+    # => redirect to user_path (dashboard)
+    # else
+    #   render survey
+
+    # if (@user.answer_one.present? || @user.answer_two.present? ||
+    #    @user.answer_three.present? || @user.answer_four.present? ||
+    #    @user.answer_five.present?)
+
+    # elsif
+
+    if params[:user][:survey] == "true"
+      @user.update(answer_one: params[:user][:answer_one],
+                   answer_two: params[:user][:answer_two],
+                   answer_three: params[:user][:answer_three],
+                   answer_four: params[:user][:answer_four],
+                   answer_five: params[:user][:answer_five])
+      if @user.profiles.count.zero?
+        redirect_to profile_choose_path
+      else
+        redirect_to user_path(@user)
+      end
+    elsif params[:user][:profileanimal].present?
+      if @user.profiles.count >= 1
+        @user.profiles.last.destroy
+        Profile.create(animal: params[:user][:profileanimal],
+                       adjective: params[:user][:profileadj],
+                       description: params[:user][:profiledesc],
+                       image: params[:user][:profileimg],
+                       user_id: current_user.id)
+      else
+        Profile.create(animal: params[:user][:profileanimal],
+                       adjective: params[:user][:profileadj],
+                       description: params[:user][:profiledesc],
+                       image: params[:user][:profileimg],
+                       user_id: current_user.id)
+      end
       redirect_to user_path(@user)
     else
       render :survey
     end
   end
+
+    # if params[:user][:profileanimal].present?
+    #   Profile.create(animal: params[:user][:profileanimal], adjective: params[:user][:profileadj],
+    #                description: params[:user][:profiledesc],
+    #                image: params[:user][:profileimg], user_id: current_user.id)
+    # else
+    #   unless @user.answer_one.present? || @user.answer_two.present? ||
+    #          @user.answer_three.present? || @user.answer_four.present? ||
+    #          @user.answer_five.present?
+    #     @user.update(answer_one: params[:user][:answer_one], answer_two: params[:user][:answer_two],
+    #                  answer_three: params[:user][:answer_three], answer_four: params[:user][:answer_four],
+    #                  answer_five: params[:user][:answer_five])
+    #     if @user.answer_one.zero? || @user.answer_two.zero? ||
+    #        @user.answer_three.zero? || @user.answer_four.zero? ||
+    #        @user.answer_five.zero?
+    #       @user.update(answered: false)
+    #     else
+    #       @user.update(answered: true)
+    #     end
+    #   end
+    # end
+    # if @user.save
+    # raise
+    #   redirect_to user_path(@user)
+    # else
+      # render :survey
+    # end
+  # end
 
   def survey
     @user = current_user
@@ -53,8 +111,8 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:age_range, :user, :gender,
-                                 :answer_one, :answered, :answer_two, :answer_three,
-                                 :answer_four, :answer_five, :d_answer_one,
-                                 :d_answer_two, :d_answer_three)
+                                 :answer_one, :answered, :answer_two,
+                                 :answer_three, :answer_four, :answer_five,
+                                 :d_answer_one, :d_answer_two, :d_answer_three)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :chatrooms, only: [:show] do
     resources :messages, only: [:create]
   end
+  get "customize_user", to: "profiles#new", as: :profile_choose
 end
