9e52b3e5e8f892d7f24dae9b15eaa16d781e0a7b
Patrick Fitzgerald <patrick.fitzgerald29@gmail.com>
2019-07-30 11:00:46 +0200

    journal and post models created

---
+++ db/migrate/20190730085725_create_journals.rb
@@ -0,0 +1,9 @@
+class CreateJournals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :journals do |t|
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
