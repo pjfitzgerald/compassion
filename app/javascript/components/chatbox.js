30c0e17b1ee7f6cd28b5186196342b2441613ab3
William Wik <william.wik@isobar.com>
2019-07-30 18:24:07 +0200

    Chatroom - A lot styling and some functionality. Unfinished

--- app/javascript/components/chatbox.js
+++ app/javascript/components/chatbox.js
@@ -1,6 +1,9 @@
 function moveUpChatbox() {
-  var sendMessageBox = document.querySelector('.create-message');
+  var sendMessageBox = document.querySelector('.messages');
   sendMessageBox.classList.toggle("expandedMessageBox");
+
+  var addHeight = document.querySelector(".chat-message-box");
+  addHeight.classList.toggle("extraHeight");
 }
 
 export { moveUpChatbox }
